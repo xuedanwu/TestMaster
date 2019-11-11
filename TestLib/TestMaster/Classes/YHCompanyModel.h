6b13735767b6770ffdd8936c871b9d7a9993eeac
xuedanwu <15618025967@163.com>
2019-11-12 01:48:48 +0800

    asdas

---
+++ TestLib/TestMaster/Classes/YHCompanyModel.h
@@ -0,0 +1,32 @@
+//
+//  YHCompanyModel.h
+//  YinheShe
+//
+//  Created by Damon on 2019/3/8.
+//  Copyright © 2019 YinHeShe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YHCompanyModel : NSObject
+
+@property (nonatomic, copy) NSString *desc;
+
+@property (nonatomic, copy) NSString *title;
+
+@property (nonatomic, copy) NSString *img;
+
+@property (nonatomic, copy) NSString *status;
+
+@property (nonatomic, copy) NSString *url;
+
+@property (nonatomic, copy) NSString *tel;
+
+@property (nonatomic, copy) NSString *contact;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestLib/TestMaster/Classes/YHCompanyModel.m
@@ -0,0 +1,14 @@
+//
+//  YHCompanyModel.m
+//  YinheShe
+//
+//  Created by Damon on 2019/3/8.
+//  Copyright © 2019 YinHeShe. All rights reserved.
+//
+
+#import "YHCompanyModel.h"
+
+@implementation YHCompanyModel
+
+
+@end
